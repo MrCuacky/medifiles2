e49c0ff324d12c5764013cb03fbf671e75245b95
MrCuacky <100604780+MrCuacky@users.noreply.github.com>
2023-03-12 01:20:14 -0600

    1

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,7 +7,9 @@ function Form() {
 
   return (
     <form class="w-full max-w-lg">
-      <div class="flex flex-wrap -mx-3 mb-6">
+
+      <div class="tracking-wide text-3xl font-bold mb-6 text-center">Información Personal del Paciente</div>
+      <div class="flex flex-wrap -mx-3 mb-2 ">
         <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
           <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-nombres">
             Nombre(s)
@@ -19,6 +21,30 @@ function Form() {
             Apellido(s)
           </label>
           <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Perez" required />
+        </div>
+      </div>
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-genero">
+            Genero
+          </label>
+          <div class="relative">
+            <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state" required>
+              <option>--</option>
+              <option>Masculino</option>
+              <option>Femenino</option>
+              <option>Otro</option>
+            </select>
+            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+              <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
+            </div>
+          </div>
+        </div>
+        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-6">
+          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
+            Edad
+          </label>
+          <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="ej:22" required />
         </div>
       </div>
       <div class="flex flex-wrap -mx-3 mb-6">
