b23e32d3595f2013dbcb025e48c1c3d0f708747c
MrCuacky <100604780+MrCuacky@users.noreply.github.com>
2023-03-12 01:38:11 -0600

    12-03-01:38am

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -23,7 +23,7 @@ function Form() {
           <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Perez" required />
         </div>
       </div>
-      <div class="flex flex-wrap -mx-3 mb-6">
+      <div class="flex flex-wrap -mx-3 mb-7">
         <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
           <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-genero">
             Genero
@@ -47,6 +47,7 @@ function Form() {
           <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="ej:22" required />
         </div>
       </div>
+      <div class="tracking-wide text-3xl font-bold mb-6 text-center">Información Curricular del Paciente</div>
       <div class="flex flex-wrap -mx-3 mb-6">
         <div class="w-full px-3">
           <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-carrera">
@@ -63,7 +64,7 @@ function Form() {
               <option>TSU en Mecatrónica Área Sistemas de Manufactura Flexible</option>
               <option>TSU en Operaciones Comerciales Internacionales Área Clasificación Arancelaria y Despacho Aduanero</option>
               <option>TSU en Procesos Industriales Área Manufactura</option>
-              <option>TSU en Tecnologia de la Información</option>
+              <option>TSU en Tecnologias de la Información</option>
               <option>Ingeniería en desarrollo y gestión de software</option>
               <option>Ingeniería en energías renovables</option>
               <option>Ingeniería en logística internacional</option>
@@ -78,14 +79,14 @@ function Form() {
           </div>
         </div>
       </div>
-      <div class="flex flex-wrap -mx-3 mb-2">
-        <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+      <div class="flex flex-wrap -mx-3 mb-7">
+        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
           <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-cuatri">
             cuatrimestre
           </label>
           <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-cuatri" type="text" placeholder="ej:5to" required />
         </div>
-        <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
           <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
             ¿Requiere ayuda?
           </label>
@@ -101,13 +102,15 @@ function Form() {
           </div>
           <p class="text-gray-600 text-xs italic">¿Requiere ayuda médica?</p>
         </div>
-        <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+        {/* <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
           <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
             Edad
           </label>
           <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="ej:22" required />
-        </div>
+        </div> */}
       </div>
+
+      <div class="tracking-wide text-3xl font-bold mb-6 text-center">Historia Médica</div>
       <div class="flex flex-wrap -mx-3 mb-6">
         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
           Padecimiento
